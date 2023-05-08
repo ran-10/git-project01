3a0638ba04dffcf2339fa1d9873f3959065a2c08
wp <2867113440@qq.com>
2023-05-08 08:41:55 +0800

    wp hotfix first modify!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! wp master second commit!");
         System.out.println("Hello Git! wp hotfix first commit!");
+        System.out.println("Hello Git! wp hotfix first modify!");
     }
 }
