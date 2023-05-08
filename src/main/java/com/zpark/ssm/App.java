e69b615d6eaf753b47b8c15fb2098b9513b65df4
wp <2867113440@qq.com>
2023-05-08 08:35:09 +0800

    wp hotfix first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! wp master second commit!");
+        System.out.println("Hello Git! wp hotfix first commit!");
     }
 }
