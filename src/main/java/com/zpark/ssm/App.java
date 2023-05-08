8a71dccf82cec367757d1cc7370201f8d74a76e8
wp <2867113440@qq.com>
2023-05-08 08:27:12 +0800

    wp master second commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,5 +3,6 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git! wp master second commit!");
     }
 }
